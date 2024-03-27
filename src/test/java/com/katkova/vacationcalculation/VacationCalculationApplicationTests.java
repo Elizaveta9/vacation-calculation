8f8630075b3dbe138dcc32a326e5547cbb87db82
Елизавета <katkova_lizochka@bk.ru>
2024-03-27 21:50:10 +0300

    Добавлен тест

--- src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
+++ src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
@@ -1,13 +1,26 @@
 package com.katkova.vacationcalculation;
 
+import com.katkova.vacationcalculation.dto.CalculationDto;
+import com.katkova.vacationcalculation.dto.CalculationResultDto;
+import com.katkova.vacationcalculation.service.CalculationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VacationCalculationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Вычисление отпускных")
+	void calculateTest() {
+		CalculationService service = new CalculationService();
+		CalculationDto calculations = new CalculationDto(50000.0F, 10);
+
+		CalculationResultDto result = service.calculate(calculations);
+
+		Assertions.assertEquals(17064.846F, result.getResult());
 	}
 
 }
