5dae4248dd5fb5c5354bd1aa89d7a65072708c59
Елизавета <katkova_lizochka@bk.ru>
2024-03-28 13:36:16 +0300

    Добавлен вычет выходных

--- src/main/java/com/katkova/vacationcalculation/service/CalculationService.java
+++ src/main/java/com/katkova/vacationcalculation/service/CalculationService.java
@@ -4,11 +4,22 @@ import com.katkova.vacationcalculation.dto.CalculationDto;
 import com.katkova.vacationcalculation.dto.CalculationResultDto;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 @Service
 public class CalculationService {
     public CalculationResultDto calculate(CalculationDto calculationDto) {
-
-        double result = calculationDto.getAvgSalary() / 29.3 * calculationDto.getVacationDays();
+        LocalDate firstDay = calculationDto.getFirstVacationDay();
+        int days = 0;
+        for (int i = 0; i < calculationDto.getVacationDays(); i++) {
+            if (firstDay.plusDays(i).getDayOfWeek() != DayOfWeek.SATURDAY &&
+                    firstDay.plusDays(i).getDayOfWeek() != DayOfWeek.SUNDAY) {
+                days++;
+            }
+        }
+        System.out.println(days);
+        double result = calculationDto.getAvgSalary() / 29.3 * days;
         return new CalculationResultDto(Math.round(result * 100) / 100.00);
     }
 }
--- src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
+++ src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
@@ -6,21 +6,22 @@ import com.katkova.vacationcalculation.service.CalculationService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class VacationCalculationApplicationTests {
 
-	@Test
-	@DisplayName("Вычисление отпускных")
-	void calculateTest() {
-		CalculationService service = new CalculationService();
-		CalculationDto calculations = new CalculationDto(50000.0, 10);
+    @Test
+    @DisplayName("Вычисление отпускных")
+    void calculateTest() {
+        CalculationService service = new CalculationService();
+        CalculationDto calculations = new CalculationDto(50000.0, 10, LocalDate.of(2024, 3, 28));
 
-		CalculationResultDto result = service.calculate(calculations);
+        CalculationResultDto result = service.calculate(calculations);
 
-		Assertions.assertEquals(17064.85, result.getResult());
-	}
+        Assertions.assertEquals(11945.39, result.getResult());
+    }
 
 }
