37945d0ac11164882d33ffd9cca5d67f7d6190dc
Елизавета <katkova_lizochka@bk.ru>
2024-03-28 13:57:49 +0300

    Добавлен тест для ввода 0 дней

--- src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
+++ src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
@@ -24,4 +24,15 @@ class VacationCalculationApplicationTests {
         Assertions.assertEquals(11945.39, result.getResult());
     }
 
+    @Test
+    @DisplayName("Вычисление отпускных если указано 0 дней")
+    void calculate0daysTest() {
+        CalculationService service = new CalculationService();
+        CalculationDto calculations = new CalculationDto(50000.0, 0, LocalDate.of(2024, 3, 28));
+
+        CalculationResultDto result = service.calculate(calculations);
+
+        Assertions.assertEquals(0.0, result.getResult());
+    }
+
 }
