e35dd7b74f8f9db2ac97a9fad32f79537e80cdc4
Елизавета <katkova_lizochka@bk.ru>
2024-03-28 14:59:47 +0300

    Добавлено вычитание праздничных дней

--- src/main/java/com/katkova/vacationcalculation/service/CalculationService.java
+++ src/main/java/com/katkova/vacationcalculation/service/CalculationService.java
@@ -6,20 +6,47 @@ import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class CalculationService {
+    private LocalDate[] holidaysArr = {
+            LocalDate.parse("2024-01-01"),
+            LocalDate.parse("2024-01-02"),
+            LocalDate.parse("2024-01-03"),
+            LocalDate.parse("2024-01-04"),
+            LocalDate.parse("2024-01-05"),
+            LocalDate.parse("2024-01-06"),
+            LocalDate.parse("2024-01-07"),
+            LocalDate.parse("2024-01-08"),
+            LocalDate.parse("2024-02-23"),
+            LocalDate.parse("2024-03-08"),
+            LocalDate.parse("2024-05-01"),
+            LocalDate.parse("2024-05-09"),
+            LocalDate.parse("2024-06-12"),
+            LocalDate.parse("2024-11-04")
+    };
+    private Set<LocalDate> holidaysSet = new HashSet<>(List.of(holidaysArr));
+
     public CalculationResultDto calculate(CalculationDto calculationDto) {
         LocalDate firstDay = calculationDto.getFirstVacationDay();
         int days = 0;
         for (int i = 0; i < calculationDto.getVacationDays(); i++) {
-            if (firstDay.plusDays(i).getDayOfWeek() != DayOfWeek.SATURDAY &&
-                    firstDay.plusDays(i).getDayOfWeek() != DayOfWeek.SUNDAY) {
+            LocalDate currentDay = firstDay.plusDays(i);
+            if (currentDay.getDayOfWeek() != DayOfWeek.SATURDAY &&
+                    currentDay.getDayOfWeek() != DayOfWeek.SUNDAY &&
+                    holidaysSet.stream().noneMatch(t ->
+                            t.getDayOfMonth() == currentDay.getDayOfMonth() &&
+                                    t.getMonth() == currentDay.getMonth()
+                    )
+            ) {
                 days++;
             }
         }
-        System.out.println(days);
         double result = calculationDto.getAvgSalary() / 29.3 * days;
         return new CalculationResultDto(Math.round(result * 100) / 100.00);
     }
+
 }
--- src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
+++ src/test/java/com/katkova/vacationcalculation/VacationCalculationApplicationTests.java
@@ -4,6 +4,7 @@ import com.katkova.vacationcalculation.dto.CalculationDto;
 import com.katkova.vacationcalculation.dto.CalculationResultDto;
 import com.katkova.vacationcalculation.service.CalculationService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +13,12 @@ import java.time.LocalDate;
 
 @SpringBootTest
 class VacationCalculationApplicationTests {
+    CalculationService service = new CalculationService();
 
     @Test
     @DisplayName("Вычисление отпускных")
     void calculateTest() {
-        CalculationService service = new CalculationService();
-        CalculationDto calculations = new CalculationDto(50000.0, 10, LocalDate.of(2024, 3, 28));
+        CalculationDto calculations = new CalculationDto(50000.0, 10, LocalDate.parse("2024-03-28"));
 
         CalculationResultDto result = service.calculate(calculations);
 
@@ -27,12 +28,31 @@ class VacationCalculationApplicationTests {
     @Test
     @DisplayName("Вычисление отпускных если указано 0 дней")
     void calculate0daysTest() {
-        CalculationService service = new CalculationService();
-        CalculationDto calculations = new CalculationDto(50000.0, 0, LocalDate.of(2024, 3, 28));
+        CalculationDto calculations = new CalculationDto(50000.0, 0, LocalDate.parse("2024-03-28"));
 
         CalculationResultDto result = service.calculate(calculations);
 
         Assertions.assertEquals(0.0, result.getResult());
+    }
+
+    @Test
+    @DisplayName("Вычисление отпускных c праздником")
+    void calculateWithHolidayTest() {
+        CalculationDto calculations = new CalculationDto(50000.0, 7, LocalDate.parse("2024-05-07"));
+
+        CalculationResultDto result = service.calculate(calculations);
+
+        Assertions.assertEquals(6825.94, result.getResult());
+    }
+
+    @Test
+    @DisplayName("Вычисление для дат другого года")
+    void calculateDifferentYearTest() {
+        CalculationDto calculations = new CalculationDto(50000.0, 7, LocalDate.parse("2025-05-07"));
+
+        CalculationResultDto result = service.calculate(calculations);
+
+        Assertions.assertEquals(6825.94, result.getResult());
     }
 
 }
